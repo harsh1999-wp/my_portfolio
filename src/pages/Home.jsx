d7a1424871ed1d6396989566d669338a3f95618d
Harsh kandoi <harshkandoi8@gmail.com>
2025-12-14 13:12:05 +0530

    final change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,26 @@
-import { Navbar } from "../components/NavBar"
-import { StarBackground } from "../components/StarBackground"
-import { ThemeToggle } from "../components/ThemeToggle"
-import { HeroSection } from "../components/HeroSection"
-import { AboutmeSection } from "../components/AboutmeSection"
-import { SkillsSection } from "../components/SkillsSection"
-import { ProjectSection } from "../components/ProjectSection"
-import { ContactSection } from "../components/ContactSection"
+import { Navbar } from "../components/NavBar.jsx";
+import { StarBackground } from "../components/StarBackground.jsx";
+import { ThemeToggle } from "../components/ThemeToggle.jsx";
+import { HeroSection } from "../components/HeroSection.jsx";
+import { AboutmeSection } from "../components/AboutmeSection.jsx";
+import { SkillsSection } from "../components/SkillsSection.jsx";
+import { ProjectSection } from "../components/ProjectSection.jsx";
+import { ContactSection } from "../components/ContactSection.jsx";
 
-export const Home = () =>{
-    return <div className="min-h-screen bg-background text-background overflow-x-hidden">
-    <ThemeToggle/>
-    <StarBackground/>
-    <Navbar/>
+export const Home = () => {
+  return (
+    <div className="min-h-screen bg-background text-background overflow-x-hidden">
+      <ThemeToggle />
+      <StarBackground />
+      <Navbar />
 
-    <main>
-        <HeroSection/>
-        <AboutmeSection/>
-        <SkillsSection/>
-        <ProjectSection/>
-        <ContactSection/>
-    </main>
+      <main>
+        <HeroSection />
+        <AboutmeSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
     </div>
-}
+  );
+};
